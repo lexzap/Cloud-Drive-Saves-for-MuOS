6199af717d0b85669981d8cba462358616eed0c0
Alex Garcia <1286840+lexzap@users.noreply.github.com>
2025-06-20 14:57:54 -0500

    pathing was incorrect for downloads

--- Cloud_Download_Saves.sh
+++ Cloud_Download_Saves.sh
@@ -12,8 +12,8 @@ echo "$0 $*"
 #LD_PRELOAD=/run/muos/storage/lib/libpadsp.so /run/muos/storage/bin/infoPanel -t "Downloading Saves" -m "Your saves are being downloaded from Dropbox!" --auto &
 
 # Synchronize saves
-/mnt/mmc/MUOS/tools/rclone copy -P -L --no-check-certificate dropbox:/ambernic/saves/ /run/muos/storage/save/ --config=/run/muos/storage/tools/rclone.conf
+/mnt/mmc/MUOS/tools/rclone copy -P -L --no-check-certificate dropbox:/ambernic/saves/ /run/muos/storage/save/ --config=/mnt/mmc/MUOS/tools/rclone.conf
 
 # Synchronize screenshots
-/mnt/mmc/MUOS/tools/rclone copy -P -L --no-check-certificate dropbox:/ambernic/screenshot/ /run/muos/storage/screenshot/ --config=/run/muos/storage/tools/rclone.conf
+/mnt/mmc/MUOS/tools/rclone copy -P -L --no-check-certificate dropbox:/ambernic/screenshot/ /run/muos/storage/screenshot/ --config=/mnt/mmc/MUOS/tools/rclone.conf
 
